d7a161a95b05ecaa49367a66a038be96a11df658
Aejie <aejiegersabalino.scc@gmail.com>
2025-08-28 09:28:29 +0000

    Seatwork7&8

--- Seatwork8.java
+++ Seatwork8.java
@@ -1,26 +1,25 @@
-public class Seatwork7 {
+public class Seatwork8 {
         public static void main(String args[]) {
-                char grade = 'C';
+                int grade = 3;
 
                         switch(grade) {
-                                    case 'A' :
+                                    case 1:
                                                     System.out.println("Excellent!");
                                                                     break;
-                                                                                case 'B' :
-                                                                                            case 'C' :
+                                                                                case 2:
+                                                                                            case 3:
                                                                                                             System.out.println("Well done");
                                                                                                                             break;
-                                                                                                                                        case 'D' :
+                                                                                                                                        case 4:
                                                                                                                                                         System.out.println("You passed");
                                                                                                                                                                         break;
-                                                                                                                                                                                    case 'F' :
+                                                                                                                                                                                    case 5:
                                                                                                                                                                                                     System.out.println("Better try again");
                                                                                                                                                                                                                     break;
-                                                                                                                                                                                                                                default :
+                                                                                                                                                                                                                                default:
                                                                                                                                                                                                                                                 System.out.println("Invalid grade");
                                                                                                                                                                                                                                                         }
                                                                                                                                                                                                                                                                 System.out.println("Your grade is " + grade);
                                                                                                                                                                                                                                                                     }
                                                                                                                                                                                                                                                                     }
-                                                                                                                                                                                                                                                                    
 }
