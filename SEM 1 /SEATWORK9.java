cd99bc3e16a4f35f3171f28881b5b1d6c1d64fb9
Aejie <aejiegersabalino.scc@gmail.com>
2026-02-28 09:37:12 +0800

    SEATWORK9.java

--- SEM 1 /SEATWORK9.java
+++ SEM 1 /SEATWORK9.java
@@ -1,14 +1,13 @@
 public class SEATWORK9 {
-public static void main(String[] args) {
-    System.out.println("GERSABALINO AEJIE");
-    int[] numbers = {10,20,30,40,50};
-    int index = 0;
+    public static void main(String[] args) {
+        System.out.println("GERSABALINO AEJIE");
+        
+        int[] numbers = {10, 20, 30, 40, 50};
+        int index = 0;
 
-    while( index < 5 ) {
-        System.out.println("Value of item: " + numbers[index]);
-        index++;
-        System.out.println("\n");
-    }
-
+        while (index < numbers.length) {
+            System.out.println("Value of item: " + numbers[index]);
+            index++;
+        }
     }  
 }
