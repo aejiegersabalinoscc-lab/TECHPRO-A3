c707ddd069de79ce4065ea4696a051a180a61001
Aejie <aejiegersabalino.scc@gmail.com>
2026-02-28 09:00:07 +0800

    SEATWORK6.java

---
+++ SEM 2 /SEATWORK6.java
@@ -0,0 +1,22 @@
+   class SEATWORK6 {
+	String name;
+	
+	void displayName() {
+		System.out.println("Worker name: " + name);
+	}
+}
+public class Seatwork6 {
+
+public static void main(String[ ]args) {
+  	
+  	
+  	Worker w1 = new Worker( );
+  	w1.name = "XiaoPang Lee Quaila";
+  	
+      try {
+      	w1.displayName( );
+      } catch (Exception e) {
+      	System.out.println("Error: Worker Inputed isnt registered");
+      }
+  }
+}
