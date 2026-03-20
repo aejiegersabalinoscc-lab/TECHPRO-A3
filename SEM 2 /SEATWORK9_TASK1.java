b5142e49f6a6fa43f2202c356ea730163d8a74b3
Aejie <aejiegersabalino.scc@gmail.com>
2026-03-21 07:22:13 +0800

    SEATWORK9_TASK1

--- SEM 2 /SEATWORK9_TASK1.java
+++ SEM 2 /SEATWORK9_TASK1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SEATWORK9_TASK9 {
+public class SEATWORK9_TASK1 {
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
         double weight;
